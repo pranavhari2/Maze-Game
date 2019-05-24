e92dc44593a99311d275445250811e724b43c57d
Pranav Hari <42246450+pranavhari2@users.noreply.github.com>
2019-05-24 15:06:19 -0400

    Add files via upload

---
+++ Player.cpp
@@ -0,0 +1,12 @@
+#include "Player.h"
+#include "Being.h"
+
+Player::Player(string _name, int _health, int _strength):Being::Being(_name, _health, _strength)
+{
+    //ctor
+}
+
+Player::~Player()
+{
+    //dtor
+}
