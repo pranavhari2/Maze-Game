812d496e0e97027c2681893ad8a2c6a2cefb6637
Pranav Hari <42246450+pranavhari2@users.noreply.github.com>
2019-05-27 19:44:27 -0400

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,22 +1,20 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include "Being.h"
-#include <iostream>
-#include <cstdlib>
 
 using namespace std;
-/*
+
 class Player : public Being
 {
     public:
         Player(string, int, int);
         virtual ~Player();
-        int movePlayer();
+        void movePlayer();
 
     protected:
 
     private:
 };
-*/
+
 
 #endif // PLAYER_H
