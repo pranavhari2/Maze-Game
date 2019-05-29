acf4f302b23a59dcfe194915b76db1a19ffea16e
Pranav Hari <42246450+pranavhari2@users.noreply.github.com>
2019-05-29 13:41:48 -0400

    Update Maze Game Summative.cpp

--- Maze Game Summative.cpp
+++ Maze Game Summative.cpp
@@ -9,18 +9,46 @@
 #include "Stack.h"
 using namespace std;
 
+void AutoSolver(Maze, Player, Stack);
+
+void AutoSolver(Maze maze, Player player, Stack stak)
+{
+    Link* temp = stak.pop();
+    string dir = temp->getContents();
+
+    cout << "DIRECTION: " << dir << endl;
+
+    return;
+}
+
+
 
 int main()
 {
     Maze maze;
+    Stack stak;
     Player player("Pranav", 100, 100);
     Monster monster("Monstah", 100, 100);
 
-    maze.setLocation(1,1);
+    maze.setSpawn(1,1);
     maze.setExit(13,13);
 
-    player.setxLocation(5);
-    player.setyLocation(5);
+    player.setxLocation(4);
+    player.setyLocation(4);
+
+
+
+      player.movePlayer(maze,"R");
+
+    //stak.reloadStack();
+
+    //AutoSolver(maze,player,stak);
+
+
+    //AutoSolver(maze);
+    Sleep(1000);
+
+    /*
 
     char key;
     int asciival;
@@ -32,35 +60,35 @@ int main()
         key = getch();
         asciival = key;
 
+        maze.displayMaze();
+
         if (asciival == 119)
         {
-            maze[]
+            player.movePlayer("up");
         }
 
         else if (asciival == 100)
         {
-
+            player.movePlayer("down");
         }
 
         else if (asciival == 115)
         {
-
+            player.movePlayer("left");
         }
 
         else if (asciival == 97)
         {
-
+            player.movePlayer("right");
         }
 
-        cout << asciival;
-
+        maze.displayMaze();
 
     }
 
+    */
 
-
-
-    //maze.displayMaze();
+    maze.displayMaze();
 
 
 
@@ -71,8 +99,6 @@ int main()
     //===========================================
 
     // User Solver
-
-
 
 
 }
