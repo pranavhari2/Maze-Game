c87bfce483bc59383c4a1d89a19ab125fc417730
Pranav Hari <42246450+pranavhari2@users.noreply.github.com>
2019-06-02 21:55:51 -0400

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -5,85 +5,12 @@
 
 Player::Player(string _name, int _health, int _strength):Being::Being( _name, _health, _strength)
 {
-    //ctor
+    xLocation = 1;
+    yLocation = 1;
 }
 
 Player::~Player()
 {
     //dtor
-}
-
-int Player::movePlayer(Maze maze, string dir)
-{
-    int val;
-
-    if (dir == "U")
-    {
-        /*
-        val = maze.getParticularMazeLocation(xLocation, yLocation-1);
-        cout << val;
-
-        if (val == 1)
-        {
-            return 0;
-        }
-        */
-
-        yLocation++;
-        return yLocation;
-
-    }
-
-    else if (dir == "D")
-    {
-        /*
-        val = maze.getParticularMazeLocation(xLocation, yLocation+1);
-        cout << val;
-
-        if (val == 0)
-        {
-            return 0;
-        }
-        */
-
-         yLocation--;
-         return yLocation;
-    }
-
-    else if (dir == "L")
-    {
-        /*
-        val = maze.getParticularMazeLocation(xLocation-1, yLocation);
-        cout << val;
-
-        if (val == 0)
-        {
-            return 0;
-        }
-        */
-
-
-         xLocation--;
-         return xLocation;
-    }
-
-    else if (dir == "R")
-    {
-        /*
-        val = maze.getParticularMazeLocation(xLocation+1, yLocation);
-        cout << val;
-
-        if (val == 0)
-        {
-            return 0;
-        }
-        */
-
-         xLocation++;
-         return xLocation;
-
-    }
-
-    return 0;
 }
 
