81cb37b15cef0de8cd5def16c35f15cdc0e6a589
Pranav Hari <42246450+pranavhari2@users.noreply.github.com>
2019-06-04 23:54:35 -0400

    Update Maze.h

--- Maze.h
+++ Maze.h
@@ -4,8 +4,6 @@
 #include <string>
 using namespace std;
 
-char const sdwl = 196;
-char const wall = 179;
 char const blnk = ' ';
 char const crn1 = 218;
 char const crn2 = 191;
@@ -13,8 +11,9 @@ char const crn3 = 192;
 char const crn4 = 217;
 char const midd = 194;
 char const sdmd = 195;
-char const whit = 210;
+char const whit = 220;
 char const whit2 = 219;
+char const whit3 = 223;
 
 
 
@@ -23,23 +22,25 @@ class Maze
 {
 
     public:
-        char maze[15][15] =
+        char maze[17][17] =
 
-    {       {crn1, sdwl, sdwl, sdwl, sdwl, sdwl, sdwl, sdwl, sdwl, sdwl, sdwl, sdwl, sdwl, sdwl, crn2}, //1//
-            {blnk, blnk, blnk, blnk, blnk, blnk, sdwl, crn2, blnk, blnk, blnk, blnk, blnk, blnk, wall}, //2//
-            {wall, blnk, sdwl, midd, blnk, blnk, blnk, wall, blnk, wall, blnk, wall, sdwl, blnk, wall}, //3//
-            {wall, blnk, blnk, wall, blnk, crn3, blnk, blnk, blnk, wall, blnk, wall, blnk, blnk, wall}, //4//
-            {wall, blnk, blnk, wall, blnk, blnk, blnk, blnk, blnk, wall, blnk, wall, blnk, blnk, wall}, //5//
-            {wall, blnk, crn1, blnk, blnk, sdwl, crn2, blnk, blnk, blnk, blnk, wall, blnk, blnk, wall}, //6//
-            {wall, blnk, wall, blnk, blnk, blnk, sdmd, blnk, sdwl, blnk, blnk, wall, blnk, blnk, wall}, //7//
-            {wall, blnk, crn3, sdwl, sdwl, blnk, blnk, sdwl, sdwl, crn2, blnk, wall, blnk, blnk, wall}, //8
-            {wall, blnk, blnk, sdwl, sdwl, sdwl, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, wall}, //9
-            {wall, blnk, blnk, blnk, blnk, blnk, blnk, wall, blnk, wall, blnk, crn1, sdwl, sdwl, wall}, //10
-            {wall, blnk, wall, blnk, wall, blnk, blnk, wall, blnk, wall, blnk, wall, blnk, blnk, wall}, //11
-            {wall, sdwl, crn4, blnk, wall, blnk, blnk, crn3, sdwl, crn4, blnk, wall, blnk, blnk, wall},  //12
-            {wall, sdwl, sdwl, sdwl, crn4, blnk, blnk, blnk, blnk, blnk, blnk, wall, blnk, blnk, wall},  //13
-            {wall, sdwl, sdwl, blnk, blnk, blnk, crn1, sdwl, sdwl, sdwl, blnk, blnk, blnk, blnk, wall},  //14
-            {crn3, sdwl, sdwl, sdwl, sdwl, sdwl, sdwl, sdwl, sdwl, sdwl, sdwl, sdwl, sdwl, sdwl, crn4},  //15
+    {       {whit,  whit, whit, whit, whit, whit, whit, whit, whit, whit, whit, whit, whit, whit, whit, whit, whit}, //1    //
+            {whit2, blnk, whit, whit, whit, whit, whit, whit, whit, whit, blnk, blnk, blnk, blnk, blnk, whit2, whit2}, //2//
+            {whit2, blnk, blnk, blnk, whit2, blnk, blnk, blnk, blnk, blnk, blnk, whit, whit, whit, blnk, whit2, whit2}, //3//
+            {whit2, whit, whit, blnk, whit2, blnk, whit, whit, whit, whit, blnk, whit, blnk, blnk, blnk, blnk, whit2}, //4//
+            {whit2, whit, whit, blnk, blnk, blnk, whit, blnk, blnk, blnk, blnk, whit, blnk, blnk, blnk, blnk, whit2}, //5//
+            {whit2, whit3, whit, blnk, blnk, blnk, whit, blnk, blnk, blnk, blnk, whit, blnk, whit, whit, blnk, whit2}, //6//
+            {whit2, whit, whit, whit, whit, blnk, whit, blnk, whit, blnk, blnk, whit, blnk, whit, whit, whit, whit2}, //7//
+            {whit2, whit2, blnk, blnk, blnk, blnk, whit, blnk, whit, whit, blnk, whit, blnk, blnk, blnk, whit, whit2}, //8
+            {whit2, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, whit2}, //9
+            {whit2, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, whit2}, //10
+            {whit2, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, whit2}, //11
+            {whit2, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, whit2},  //12
+            {whit2, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, whit2},  //13
+            {whit2, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, whit2},  //14
+            {whit2, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, whit2},
+            {whit2, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, blnk, whit2},
+            {whit3, whit3, whit3, whit3, whit3, whit3, whit3, whit3, whit3, whit3, whit3, whit3, whit3, whit3, whit3, whit3, whit3},  //15
     };
 
         Maze();
